ef8d98716ab548b44c6351b29f54d22f61f0da99
long12360 <154275017@qq.com>
2019-10-23 15:15:16 +0800

    2.1

--- src/main/java/com/test/Demo.java
+++ src/main/java/com/test/Demo.java
@@ -5,5 +5,7 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("21321321");
+        System.out.println("我来了");
+        System.out.println("来了 哈哈哈哈");
     }
 }
